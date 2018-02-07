2749aa85d2abea01728620e20b29d1329fe6d585
farbelo <farbelo@motionsoft.com.uy>
2018-02-07 14:36:09 -0300

    gdfgdf

---
+++ bd/good.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 07-02-2018 a las 18:34:53
+-- Versión del servidor: 5.5.8
+-- Versión de PHP: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `good`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pointofsale`
+--
+
+CREATE TABLE IF NOT EXISTS `pointofsale` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(56) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `tel` varchar(15) NOT NULL,
+  `image` varchar(56) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Volcar la base de datos para la tabla `pointofsale`
+--
+
+INSERT INTO `pointofsale` (`id`, `name`, `address`, `tel`, `image`) VALUES
+(1, 'Porto Vanilla..', 'erwrwerew', '21312321', 'porto-vanilla.png'),
+(2, 'Las Gaviotas', 'erwrwerew66', '21312321', 'las-gaviotas.png'),
+(3, 'El Viejo y El Mar', 'asdasda', '1111', 'el-viejo-y-el-mar.jpg');
