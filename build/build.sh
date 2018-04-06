2ca8cd45288507a137e5c2b9aa2875aea6aa5f1c
conergia <conergia.work@gmail.com>
2018-04-05 23:15:48 -0300

    script para levantar aplicación

---
+++ build/build.sh
@@ -0,0 +1,34 @@
+echo "***************************"
+echo "Actualizando codigo: git pull"
+git pull
+echo ""
+cd ..
+echo "***************************"
+echo "Actualizando paquetes servidor: npm install"
+npm install
+echo ""
+echo "***************************"
+echo "Compilando TypeScrip servidor: tsc"
+tsc
+cd client
+echo ""
+echo "***************************"
+echo "Actualizando paquetes servidor: npm install"
+npm install
+echo ""
+echo "***************************"
+echo "Compilando TypeScrip client: tsc"
+tsc
+echo ""
+echo "***************************"
+echo "Compilando angular: ng build"
+ng build
+echo ""
+echo "***************************"
+echo "Actualizando imagenes cp de images a client"
+cp -r ../client/images ../client/dist/
+cd ..
+echo ""
+echo "***************************"
+echo "Ejecutando el forever"
+forever start bin/www
