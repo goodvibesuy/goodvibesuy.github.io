388b583166bad97cc3853cb9e74df17ad8c80921
conergia <conergia.work@gmail.com>
2018-04-29 15:36:07 -0300

    backup de codigo

---
+++ bd/sql.29.04.2018/good.schema.sql
@@ -0,0 +1,218 @@
+DROP DATABASE IF EXISTS `good`;
+CREATE DATABASE `good`;
+USE `good`;
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `names` varchar(56) NOT NULL,
+  `lastnames` varchar(56) NOT NULL,
+  `address` varchar(68) NOT NULL,
+  `phone` varchar(68) NOT NULL,
+  `coord` point DEFAULT NULL, 
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+ `id` INT(11) NOT NULL AUTO_INCREMENT,
+ `user_name` VARCHAR(256) NOT NULL,
+ `firstname` VARCHAR(256) NOT NULL,
+ `lastname` VARCHAR(50) NOT NULL,
+ `password` VARCHAR(30) NOT NULL,
+ `rol_id` INT(11) NOT NULL,
+ `id_user_master` INT(11) NOT NULL, PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- groupPointofsale
+DROP TABLE IF EXISTS `groupPointofsale`;
+CREATE TABLE `groupPointofsale` (
+ `id` INT(11) NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(56) NOT NULL, PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- pointofsale
+DROP TABLE IF EXISTS `pointofsale`;
+CREATE TABLE `pointofsale` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(56) NOT NULL,
+    `businessName` VARCHAR(56) NOT NULL,
+    `RUT` VARCHAR(56) NOT NULL,
+    `contactName` VARCHAR(56) NOT NULL, 
+    `address` VARCHAR(256) NOT NULL,
+    `tel` VARCHAR(15) DEFAULT NULL,
+    `image` VARCHAR(56) DEFAULT NULL,
+    `coord` POINT DEFAULT NULL,
+    `idGroup` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (idGroup) REFERENCES groupPointofsale(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- product
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+ `id` INT(11) NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(56) NOT NULL,
+ `displayOrder` INT(11) DEFAULT 100 NOT NULL,
+ `path_image` VARCHAR(128) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- productprice
+DROP TABLE IF EXISTS `productprice`;
+CREATE TABLE `productprice` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `date` DATETIME NOT NULL,
+    `amount` FLOAT(11) NOT NULL,
+    `idProduct` INT(11) NOT NULL,
+    `idGroupPointofsale` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (idProduct) REFERENCES product(id),
+    FOREIGN KEY (idGroupPointofsale) REFERENCES groupPointofsale(id),
+    UNIQUE KEY `id_idProduct_idGroupPointofsale` (`id`, `idProduct`, `idGroupPointofsale`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*ALTER TABLE `productprice` CHANGE `amount` `amount` FLOAT(11) NOT NULL;
+
+    `tel` VARCHAR(15) DEFAULT NOT NULL,
+    `image` VARCHAR(56) DEFAULT NULL,
+    `coord` POINT DEFAULT NULL,
+*/
+
+-- unit
+DROP TABLE IF EXISTS `unit`;
+CREATE TABLE `unit` (
+ `id` INT(11) NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(56) NOT NULL, PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- provider
+DROP TABLE IF EXISTS `provider`;
+CREATE TABLE `provider` (
+ `id` INT(11) NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(256) NOT NULL, PRIMARY KEY (`id`)
+);
+
+-- supply
+DROP TABLE IF EXISTS `supply`;
+CREATE TABLE `supply` (
+ `id` INT(11) NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(56) NOT NULL,
+ `unit` INT(11) NOT NULL,
+ `path_image` VARCHAR(128) NULL,
+ PRIMARY KEY (`id`),
+ FOREIGN KEY (unit) REFERENCES unit(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- product_supply
+DROP TABLE IF EXISTS `product_supply`;
+CREATE TABLE `product_supply` (
+ `idProduct` INT(11) DEFAULT NULL,
+ `idSupply` INT(11) DEFAULT NULL,
+ `quantity` DOUBLE DEFAULT NULL, FOREIGN KEY (idProduct) REFERENCES product(id), FOREIGN KEY (idSupply) REFERENCES supply(id) 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- route
+DROP TABLE IF EXISTS `route`;
+CREATE TABLE `route` (
+ `id` INT(11) NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(45) DEFAULT NULL,
+ `date` DATETIME NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- viewing
+DROP TABLE IF EXISTS `viewing`;
+CREATE TABLE `viewing` (
+ `idUser` INT(11) DEFAULT NULL,
+ `idviewing` INT(11) NOT NULL AUTO_INCREMENT,
+ `date` DATETIME DEFAULT NULL,
+ `idpointofsale` INT(11) DEFAULT NULL,
+ `annotation` TEXT DEFAULT NULL, 
+ PRIMARY KEY (`idviewing`),
+ FOREIGN KEY (idUser) REFERENCES users(id),
+ FOREIGN KEY (idpointofsale) REFERENCES pointofsale(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- viewing_product
+DROP TABLE IF EXISTS `viewing_product`;
+CREATE TABLE `viewing_product` (
+ `idviewing` INT(11) NOT NULL,
+ `idproduct` INT(11) NOT NULL,
+ `quantity` INT(11) DEFAULT NULL,
+ `type` ENUM('delivery','return','empty') DEFAULT NULL, KEY `idviewing_idx` (`idviewing`), KEY `fk_viewing_product_product1_idx` (`idproduct`), CONSTRAINT `fk_viewing_product_product1` FOREIGN KEY (`idproduct`) REFERENCES `product` (`id`) ON
+DELETE NO ACTION ON
+UPDATE NO ACTION,
+CONSTRAINT `idviewing` FOREIGN KEY (`idviewing`) REFERENCES `viewing` (`idviewing`) ON
+DELETE NO ACTION ON
+UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- route_pointofsale
+DROP TABLE IF EXISTS `route_pointofsale`;
+CREATE TABLE `route_pointofsale` (
+ `idRoute` INT(11) DEFAULT NULL,
+ `idPointofsale` INT(11) DEFAULT NULL,
+ `position` INT(11) NOT NULL, 
+ `idViewing` int(11) DEFAULT NULL, 
+ KEY `idTravel_idx` (`idroute`),
+ KEY `idPointofsale_idx` (`idpointofsale`),
+ FOREIGN KEY (idroute) REFERENCES route(id), 
+ FOREIGN KEY (idPointofsale) REFERENCES pointofsale(id),
+ FOREIGN KEY (idViewing) REFERENCES viewing(idviewing)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- route_user
+DROP TABLE IF EXISTS `route_user`;
+CREATE TABLE `route_user` (
+    `idroute` INT(11) NOT NULL,
+    `iduser` INT(11) NOT NULL,
+    KEY `key_iduser` (`iduser`),
+    KEY `key_idroute` (`idroute`), 
+	FOREIGN KEY (`idroute`) REFERENCES `route` (`id`),
+	FOREIGN KEY (`iduser`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- supplyPrice
+DROP TABLE IF EXISTS `supplyPrice`;
+CREATE TABLE `supplyPrice` (
+ `price_date` DATETIME NOT NULL,
+ `date` DATETIME NOT NULL,
+ `amount` INT(11) DEFAULT NULL,
+ `idSupply` INT(11) DEFAULT NULL,
+ `idProvider` INT(11) NOT NULL,
+ KEY `idSupply_idx` (`idSupply`),
+ FOREIGN KEY (idProvider) REFERENCES `provider` (id),
+ CONSTRAINT `idSupply` FOREIGN KEY (`idSupply`) REFERENCES `supply` (`id`) ON
+DELETE NO ACTION ON
+UPDATE NO ACTION
+);
+
+-- templateRoute
+DROP TABLE IF EXISTS `templateRoute`;
+CREATE TABLE `templateRoute` (
+ `id` INT(11) NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(45) DEFAULT NULL, PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- templateRoute_pointofsale
+DROP TABLE IF EXISTS `templateRoute_pointofsale`;
+CREATE TABLE `templateRoute_pointofsale` (
+ `idTemplateRoute` INT(11) DEFAULT NULL,
+ `idpointofsale` INT(11) DEFAULT NULL,
+ `position` INT(11) NOT NULL, FOREIGN KEY (idTemplateRoute) REFERENCES templateRoute(id),
+ FOREIGN KEY (idpointofsale) REFERENCES pointofsale(id) 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `route_stock`;
+CREATE TABLE `route_stock` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `idRoute` int(11) DEFAULT NULL,
+  `idProduct` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`idRoute`) REFERENCES `route` (`id`),
+  FOREIGN KEY (idProduct) REFERENCES product(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ bd/sql.29.04.2018/master.schema.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 06-03-2018 a las 19:03:36
+-- Versión del servidor: 5.5.8
+-- Versión de PHP: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `master`
+--
+DROP DATABASE IF EXISTS `master`;
+CREATE DATABASE `master`;
+USE `master`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `accounts`
+--
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `database_name` varchar(45) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `max_users` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resources`
+--
+
+CREATE TABLE IF NOT EXISTS `resources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rols`
+--
+
+CREATE TABLE IF NOT EXISTS `rols` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rols_resources`
+--
+
+CREATE TABLE IF NOT EXISTS `rols_resources` (
+  `rol_id` int(11) NOT NULL,
+  `resource_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Estructura de tabla para la tabla `sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(120) NOT NULL,
+  `login_date` datetime NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `token_id` varchar(300) NOT NULL,
+  `database_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_name`,`login_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `rol_id` tinyint(4) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(120) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `user_name_UNIQUE` (`user_name`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `user_name_2` (`user_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=28 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users_accounts`
+--
+
+CREATE TABLE IF NOT EXISTS `users_accounts` (
+  `user_id` int(10) unsigned NOT NULL,
+  `account_id` int(10) unsigned NOT NULL,
+  `owner` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
