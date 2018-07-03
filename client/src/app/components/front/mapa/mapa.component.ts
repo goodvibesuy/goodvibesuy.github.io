756a49aa0b78a0ea08470f40a3976c2153eba2f7
farbelo <farbelo@motionsoft.com.uy>
2018-07-02 21:55:31 -0300

    descargar csv

--- client/src/app/components/front/mapa/mapa.component.ts
+++ client/src/app/components/front/mapa/mapa.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { RouteService } from '../../../services/route.service';
 import { PointOfSale } from '../../../../../../datatypes/pointOfSale';
 import { ActivatedRoute } from '@angular/router';
-import { ProductsService } from '../../../services/products.service';
 import { Product } from '../../../../../../datatypes/product';
 import { AuthenticateService } from '../../../services/authenticate.service';
 import { ViewingService } from '../../../services/viewing.service';
 import { UsersService } from '../../../services/users.service';
+import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 
 @Component({
     selector: 'app-mapa',
@@ -19,7 +19,7 @@ export class MapaComponent implements OnInit {
     private viewings: any[];
     pointsOfSale: PointOfSale[];
     finishedViewing: PointOfSale[];
-    productsDelivery: {idproduct: number, quantity: number, displayOrder: number, name: string, path_image: string}[];
+    productsDelivery: { idproduct: number, quantity: number, displayOrder: number, name: string, path_image: string }[];
     public currentRoute: number;
     public stock: { product: Product, quantity: number }[];
 
@@ -28,7 +28,7 @@ export class MapaComponent implements OnInit {
         private routeService: RouteService,
         private authenticateService: AuthenticateService,
         private viewingService: ViewingService,
-        private userService:UsersService
+        private userService: UsersService
     ) {
         this.stock = new Array<{ product: Product, quantity: number }>();
     }
@@ -41,9 +41,8 @@ export class MapaComponent implements OnInit {
                 } else {
                     this.pointsOfSale = new Array<PointOfSale>();
                     this.userService.getCurrentUser().subscribe(
-                        response =>
-                        {
-                            if (response.result > 0){
+                        response => {
+                            if (response.result > 0) {
                                 let userId = response.data[0].id;
                                 this.routeService.getRoutesByUserId(userId).subscribe(
                                     responseRoutes => {
@@ -51,33 +50,71 @@ export class MapaComponent implements OnInit {
                                     }
                                 );
                                 this.showRoute(Number(this.route.snapshot.paramMap.get('idRoute')));
-                            }                            
+                            }
                         }
-                    );                    
+                    );
                 }
             });
     }
 
+    public downloadTrip(): void {
+        var options = {
+            fieldSeparator: ';',
+            quoteStrings: '"',
+            decimalseparator: '.',
+            showLabels: true
+        };
+
+        let data: { name: string, address: string, tel: string }[] = new Array<{ name: string, address: string, tel: string }>();
+        //name, address , tel
+
+        data.push({ name: "Nombre", address: "Dirección", tel: "Telefono" });
+        for (let i = 0; i < this.pointsOfSale.length; i++) {
+            data.push({ name: this.pointsOfSale[i].name, address: this.pointsOfSale[i].address, tel: this.pointsOfSale[i].tel });
+        }
+        //finishedViewing
+        new Angular2Csv(data, 'Puntos de ventas recorridos', options);
+    }
+
+    public downloadFinished(): void {
+        var options = {
+            fieldSeparator: ';',
+            quoteStrings: '"',
+            decimalseparator: '.',
+            showLabels: true
+        };
+
+        let data: { name: string, address: string, tel: string }[] = new Array<{ name: string, address: string, tel: string }>();
+        //name, address , tel
+        data.push({ name: "Nombre", address: "Dirección", tel: "Telefono" });
+        console.log(this.finishedViewing);
+        for (let i = 0; i < this.finishedViewing.length; i++) {
+            data.push({ name: this.finishedViewing[i].pos.name, address: this.finishedViewing[i].pos.address, tel: this.finishedViewing[i].pos.tel });
+        }
+        //
+        new Angular2Csv(data, 'Puntos de ventas pendientes', options);
+    }
+
     public getFinishedViewing(): any {
         let finished = this.pointsOfSale.filter(input => input.idViewing !== null);
-        let viewingPOS:Array<{date:Date,pos:PointOfSale}>;
-        viewingPOS = new Array<{date:Date,pos:PointOfSale}>();
-        for(let i = 0;i < finished.length ; i++){
+        let viewingPOS: Array<{ date: Date, pos: PointOfSale }>;
+        viewingPOS = new Array<{ date: Date, pos: PointOfSale }>();
+        for (let i = 0; i < finished.length; i++) {
             let viewing = this.viewings.filter(v => v.idCustomer == finished[i].id)[0];
-            console.log(viewing,this.viewings);
-            viewingPOS.push({date:viewing.date,pos:finished[i]});
+            console.log(viewing, this.viewings);
+            viewingPOS.push({ date: viewing.date, pos: finished[i] });
         }
 
-        return viewingPOS.sort(function(a, b) {
+        return viewingPOS.sort(function (a, b) {
             if (a.date > b.date) {
-              return -1;
+                return -1;
             }
             if (a.date < b.date) {
-              return 1;
+                return 1;
             }
             return 0;
-          });
-        
+        });
+
     }
 
     public getUnFinishedViewing(): any {
@@ -113,7 +150,7 @@ export class MapaComponent implements OnInit {
                             this.viewingService.getRouteDelivery(idRoute).subscribe(
                                 responseDelivery => {
                                     console.log(responseDelivery);
-                                    if(responseDelivery.result > 0){
+                                    if (responseDelivery.result > 0) {
                                         this.productsDelivery = responseDelivery.data;
                                     }
                                 }
